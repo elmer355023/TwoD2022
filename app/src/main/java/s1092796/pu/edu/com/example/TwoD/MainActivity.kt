243cd2047cd0dce0ef96b25c9cba75e709cb9b08
elmer355023 <elmer355023@gmail.com>
2022-05-01 13:34:41 +0800

    test

---
+++ app/src/main/java/s1092796/pu/edu/com/example/TwoD/MainActivity.kt
@@ -0,0 +1,67 @@
+package s1092796.pu.edu.com.example.TwoD
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.coroutines.*
+import kotlinx.android.synthetic.main.activity_main.*
+import s1092796.pu.edu.com.example.TwoD.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var job: Job
+
+    lateinit var binding: ActivityMainBinding
+    var secondsLeft:Int = 1000  //倒數
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding.btnStart.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(secondsLeft > 0) {
+                        secondsLeft--
+                        binding.txv.text = secondsLeft.toString()
+                        binding.btnStart.isEnabled = false
+                        binding.btnStop.isEnabled = true
+                        delay(25)
+                    }
+                }
+            }
+        })
+
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txv.text = secondsLeft.toString()
+        binding.btnStart.isEnabled = true
+        binding.btnStop.isEnabled = false
+
+        binding.btnStop.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                job.cancel()
+                binding.btnStart.isEnabled = true
+                binding.btnStop.isEnabled = false
+            }
+        })
+
+    }
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (binding.btnStart.isEnabled == false){
+            job = GlobalScope.launch(Dispatchers.Main) {
+                while(secondsLeft > 0) {
+                    secondsLeft--
+                    binding.txv.text = secondsLeft.toString()
+                    delay(25)
+                }
+            }
+        }
+    }
+}
